19de8b6c8104316baac997f5271a9cd90e3af5ac
Mohammad Salehi <mds1121@yahoo.com>
2022-10-31 01:00:27 +0330

    override error handler for api exceptions

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,12 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\Finder\Exception\AccessDeniedException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -46,5 +51,28 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        // if ($request->expectsJson()) {
+        if ($request->is('api/*')) {
+            if ($exception instanceof ModelNotFoundException)
+                return app('res')->error('object not found', [], 404);
+
+            if ($exception instanceof AuthorizationException)
+                return app('res')->error('unauthorized.', [], 401);
+
+            if ($exception instanceof AuthenticationException)
+                return app('res')->error('Unauthenticated.', [], 401);
+
+            if ($exception instanceof AccessDeniedException)
+                return app('res')->error('Unauthorized.', [], 403);
+
+            if ($exception instanceof ValidationException)
+                return app('res')->error('validation error', $exception->validator->errors(), 422);
+        }
+
+        return parent::render($request, $exception);
     }
 }
